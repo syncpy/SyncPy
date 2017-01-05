6001a3706a3b2ff0137fb99ea8b0ce049da61c54
dareversat <david.reversat@upmc.fr>
2017-01-05 12:01:47 +0100

    SyncPy2 Linux and Mac installers

---
+++ installeurs/Linux/SyncPy-2.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Download and install Anaconda if not installed
+
+if ! [ -d "$HOME/anaconda" ]; then
+    echo "Getting Anaconda..."
+
+    MACHINE_TYPE=`uname -m`
+    if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+        wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh --no-check-certificat
+        mv Anaconda-2.3.0-Linux-x86_64.sh Anaconda_install.sh
+    else
+        wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86.sh --no-check-certificate
+        mv Anaconda-2.3.0-Linux-x86.sh Anaconda_install.sh
+    fi
+
+
+    bash Anaconda_install.sh -b -p $HOME/anaconda
+
+    cp $HOME/.profile $HOME/.profile.syncpysave
+
+    echo -e "\n#ADDED BY SYNCPY2\nPATH=$HOME/anaconda/bin:$PATH" >> $HOME/.profile
+
+    . $HOME/.profile
+
+    rm Anaconda_install.sh
+
+else
+
+    echo "Anaconda is already installed"
+
+fi
+
+# Download SyncPy2
+echo "Getting SyncPy2..."
+wget https://github.com/syncpy/SyncPy/archive/master.zip --no-check-certificate
+
+unzip -qq master.zip
+
+# Install SyncPy2 in /
+sudo cp -R SyncPy-master /SyncPy2/
+
+sudo chmod -R 777 /SyncPy2/
+
+rm -R SyncPy-master
+rm master.zip
+
+# Create a shortcut to the interface in /home/user
+if [ -f $HOME/SyncPy2 ]; then
+    rm $HOME/SyncPy2
+fi
+touch $HOME/SyncPy2
+cd ..
+echo "#!/bin/bash" >> $HOME/SyncPy2
+echo 'if ! [[ $PATH == *"anaconda"* ]]; then . .profile; fi' >> $HOME/SyncPy2
+echo "cd /SyncPy2/src/" >> $HOME/SyncPy2
+echo "python SyncPy2.py" >> $HOME/SyncPy2
+
+sudo chmod +x $HOME/SyncPy2
+
+# Create a shortcut to the module wizard in /home/user
+if [ -f $HOME/SyncPy2MethodWizard ]; then
+    rm $HOME/SyncPy2MethodWizard
+fi
+touch $HOME/SyncPy2MethodWizard
+cd ..
+echo "#!/bin/bash" >> $HOME/SyncPy2MethodWizard
+echo 'if ! [[ $PATH == *"anaconda"* ]]; then . .profile; fi' >> $HOME/SyncPy2MethodWizard
+echo "cd /SyncPy2/src/" >> $HOME/SyncPy2MethodWizard
+echo "python SyncPy2MethodWizard.py" >> $HOME/SyncPy2MethodWizard
+
+sudo chmod +x $HOME/SyncPy2MethodWizard
+
+echo "SyncPy2 installed in /SyncPy2/ - Shortcuts created in the home folder"
